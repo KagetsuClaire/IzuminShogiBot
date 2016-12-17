1019f44b2a268b609faf47542a5acd1ec2d94f10
Kagetsu <kagetsu.claire@gmail.com>
2016-12-17 21:44:15 +0900

    Change timezone

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from Twitter import tweet
 
 while True:
     now = datetime.datetime.now()
-    # UTC 19:00 == 日本時間4:00
-    if now.minute == 0 and now.hour % 4 == 3 and now.hour != 19:
+    if now.minute == 0 and now.hour % 4 == 0 and now.hour != 4:
         tweet.random_update()
     time.sleep(60)
 else:
