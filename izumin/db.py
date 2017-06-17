1657d3776377205b32a88eb2c930320306fd27cc
Kagetsu <kagetsu.claire@gmail.com>
2017-06-18 02:15:31 +0900

    Delete test code

--- izumin/db.py
+++ izumin/db.py
@@ -9,10 +9,3 @@ DATABASE_USER = url.username
 DATABASE_PASSWORD = url.password
 DATABASE_HOST = url.hostname
 DATABASE_PORT = url.port
-
-print("DB connection")
-print("name: ", DATABASE_NAME)
-print("user: ", DATABASE_USER)
-print("password: ", DATABASE_PASSWORD)
-print("host: ", DATABASE_HOST)
-print("port: ", DATABASE_PORT)
