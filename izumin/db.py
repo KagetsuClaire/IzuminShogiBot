932e106213ee33cebddebb49b7ff192d15f20de4
Kagetsu <kagetsu.claire@gmail.com>
2017-06-18 02:08:53 +0900

    test

--- izumin/db.py
+++ izumin/db.py
@@ -9,3 +9,10 @@ DATABASE_USER = url.username
 DATABASE_PASSWORD = url.password
 DATABASE_HOST = url.hostname
 DATABASE_PORT = url.port
+
+print("DB connection")
+print("name: ", DATABASE_NAME)
+print("user: ", DATABASE_USER)
+print("password: ", DATABASE_PASSWORD)
+print("host: ", DATABASE_HOST)
+print("port: ", DATABASE_PORT)
